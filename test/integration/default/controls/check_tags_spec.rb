0f5e21ec468c5361b2c5da92ad6dfc415a6fe3ea
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2018-11-05 14:27:41 +0200

    Fix kitchen test for tags

--- test/integration/default/controls/check_tags_spec.rb
+++ test/integration/default/controls/check_tags_spec.rb
@@ -1,7 +1,12 @@
+tags = attribute(	
+	"tags",
+	description: "Test EC2 instance tag 'Name' is not empty "
+	)
+
+
 control 'check_tag' do
-
-  describe os_env('tags') do
-    its('content') { should_not eq '' }   
-  end
-
+	describe tags do 
+	  it { should_not eq "nil" } 
+	  it { should_not eq "" }
+	end
 end
